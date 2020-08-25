0b757de1edad47dbdc9c876ec7ee50a0f491b188
Don Aldrich <aldrich.don@gmail.com>
2020-08-25 07:47:05 -0500

    initial

---
+++ gitlab/modules/project/main.tf
@@ -0,0 +1,84 @@
+
+resource "gitlab_project" "project" {
+  name                       = var.name
+  issues_enabled             = var.issues_enabled
+  lfs_enabled                = var.lfs_enabled
+  wiki_enabled               = var.wiki_enabled
+  snippets_enabled           = var.snippets_enabled
+  merge_requests_enabled     = var.merge_requests_enabled
+  container_registry_enabled = var.container_registry_enabled
+  default_branch             = var.default_branch
+  description                = var.description
+  # tags             = ["ansible"]
+}
+
+resource "gitlab_branch_protection" "master_branch_protection" {
+  branch = "master"
+  push_access_level = "maintainer"
+  merge_access_level = "maintainer"
+  project   = var.project
+}
+
+resource "gitlab_project_variable" "VAULT_ADDR" {
+    environment_scope = "*"
+    key       = "VAULT_ADDR"
+    value     = var.VAULT_ADDR
+    protected = true
+    project   = var.project
+    depends_on = [gitlab_project.project]
+}
+
+resource "gitlab_project_variable" "VAULT_TOKEN" {
+    environment_scope = "*"
+    key       = "VAULT_TOKEN"
+    value     = var.VAULT_TOKEN
+    protected = true
+    project   = var.project
+    depends_on = [gitlab_project.project]
+}
+
+resource "gitlab_project_variable" "JWT_LOGIN" {
+    environment_scope = "*"
+    key       = "JWT_LOGIN"
+    value     = "gitlab-ci"
+    protected = true
+    project   = var.project
+    depends_on = [gitlab_project.project]
+}
+
+# resource "gitlab_project_variable" "DOCKERHUB_PASS" {
+#     environment_scope = "*"
+#     key       = "DOCKERHUB_PASS"
+#     value     = var.DOCKERHUB_PASS
+#     protected = true
+#     project   = var.project
+#     depends_on = [gitlab_project.project]
+# }
+
+# resource "gitlab_project_variable" "DOCKERHUB_USER" {
+#     environment_scope = "*"
+#     key       = "DOCKERHUB_USER"
+#     value     = var.DOCKERHUB_USER
+#     protected = true
+#     project   = var.project
+#     depends_on = [gitlab_project.project]
+# }
+
+resource "gitlab_project_variable" "private-ci-vars" {
+    for_each = var.private_variables
+    environment_scope = "*"
+    key       = each.key
+    value     = each.value
+    protected = true
+    project   = var.project
+    depends_on = [gitlab_project.project]
+}
+
+resource "gitlab_pipeline_schedule" "scheduled-pipeline" {
+   project     = var.project
+   description = "Scheduled Pipeline"
+   ref         = "master"
+   cron        = var.pipeline_cron
+   active      = true
+   cron_timezone = "America/Chicago"
+}
---
+++ gitlab/modules/project/vars.tf
@@ -0,0 +1,139 @@
+variable "approvals_before_merge" {
+  type        = number
+  description = "Number of merge request approvals required for merging"
+  default     = 0
+}
+
+variable "container_registry_enabled" {
+  type        = bool
+  description = "Enable container registry for the project"
+  default     = false
+}
+
+variable "default_branch" {
+  type        = string
+  description = "The default branch for the project"
+  default     = "master"
+}
+
+variable "description" {
+  type        = string
+  description = "A description of the project"
+  default     = "Managed by Terraform"
+}
+
+variable "gitlab_token" {
+  type        = string
+  description = "This is the GitLab personal access token"
+}
+
+variable "initialize_with_readme" {
+  type        = bool
+  description = "Create master branch with first commit containing a README.md file"
+  default     = false
+}
+
+variable "issues_enabled" {
+  type        = bool
+  description = "Enable issue tracking for the project"
+  default     = false
+}
+
+variable "lfs_enabled" {
+  type        = bool
+  description = "Enable LFS for the project"
+  default     = false
+}
+
+variable "merge_requests_enabled" {
+  type        = bool
+  description = "Enable merge requests for the project"
+  default     = false
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the project"
+}
+
+variable "only_allow_merge_if_all_discussions_are_resolved" {
+  type        = bool
+  description = "Set to true if you want allow merges only if all discussions are resolved"
+  default     = false
+}
+
+variable "only_allow_merge_if_pipeline_succeeds" {
+  type        = bool
+  description = "Set to true if you want allow merges only if a pipeline succeeds"
+  default     = false
+}
+
+variable "project" {
+  type        = string
+  description = "Path of the repository"
+}
+
+variable "pipelines_enabled" {
+  type        = bool
+  description = "Enable pipelines for the project"
+  default     = true
+}
+
+variable "shared_runners_enabled" {
+  type        = bool
+  description = "Enable shared runners for this project"
+  default     = true
+}
+
+variable "snippets_enabled" {
+  type        = bool
+  description = ""
+  default     = false
+}
+
+variable "visibility_level" {
+  type        = string
+  description = "Set to public to create a public project. Valid values are private, internal, public. Repositories are created as private by default."
+  default     = "private"
+}
+
+variable "wiki_enabled" {
+  type        = bool
+  description = "Enable wiki for the project"
+  default     = false
+}
+
+variable "VAULT_TOKEN" {
+  type        = string
+  description = "Your Vault Token"
+}
+
+variable "VAULT_ADDR" {
+  type        = string
+  description = "Your Vault URL"
+}
+
+variable "DOCKERHUB_USER" {
+  type        = string
+  description = "DOCKERHUB_USER"
+}
+
+variable "DOCKERHUB_PASS" {
+  type        = string
+  description = "DOCKERHUB_PASS"
+}
+
+variable "private_variables" {
+  description = "List of private variables"
+  type        = map(string)
+  default = {
+    "DOCKERHUB_USER" = "bar"
+    "DOCKERHUB_PASS" = "baz"
+  }
+}
+
+variable "pipeline_cron" {
+  type        = string
+  description = "CRON for scheduled pipeline"
+  default = "0 6 * * *"
+}
