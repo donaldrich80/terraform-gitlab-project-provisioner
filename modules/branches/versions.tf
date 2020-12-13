84ca7b48657067575b07c16ec3a52a0586173287
Don Aldrich <aldrich.don@gmail.com>
2020-12-13 08:08:45 -0600

    versions.tf

--- modules/branches/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    gitlab = {
-      source  = "gitlabhq/gitlab"
-      version = "~> 3.0.0"
-    }
-  }
-  required_version = ">= 0.13"
-}
--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     gitlab = {
       source  = "gitlabhq/gitlab"
-      version = "~> 3.0.0"
+      version = "~> 3.3.0"
     }
   }
   required_version = ">= 0.13"
