310c6feccd745462616b7c06f6a326d82d19e5d0
Don Aldrich <aldrich.don@gmail.com>
2021-03-31 10:40:47 +0000

    chore: versions

--- modules/pipelines/versions.tf
+++ modules/pipelines/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     gitlab = {
       source  = "gitlabhq/gitlab"
-      version = "~> 3.5.0"
+      version = "3.5.0"
     }
   }
   required_version = ">= 0.14"
