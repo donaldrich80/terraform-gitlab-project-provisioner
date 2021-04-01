d5c0bb6b03f8b7cf46dd68ac1656f4b40196549f
Don Aldrich <aldrich.don@gmail.com>
2021-04-01 02:51:17 +0000

    feat: enhanced webhooks

--- modules/webhooks/main.tf
+++ modules/webhooks/main.tf
@@ -2,12 +2,41 @@ resource "gitlab_project_hook" "hook" {
   project         = var.project
   url             = var.url
   pipeline_events = var.pipeline_events
+  merge_requests_events = var.merge_requests_events
+  push_events = var.push_events
+  enable_ssl_verification = var.enable_ssl_verification
+  tag_push_events = var.tag_push_events
 }
 
 variable "pipeline_events" {
   type        = bool
   description = "pipeline_events"
   default     = false
+}
+
+variable "merge_requests_events" {
+  type        = bool
+  description = "merge_requests_events"
+  default     = false
+}
+
+variable "push_events" {
+  type        = bool
+  description = "push_events"
+  default     = false
+}
+
+variable "tag_push_events" {
+  type        = bool
+  description = "tag_push_events"
+  default     = false
+}
+
+
+variable "enable_ssl_verification" {
+  type        = bool
+  description = "enable_ssl_verification"
+  default     = true
 }
 
 variable "project" {
@@ -22,27 +51,17 @@ variable "url" {
 
 // token - (Optional) A token to present when invoking the hook.
 
-// enable_ssl_verification - (Optional) Enable ssl verification when invoking the hook.
-
-// push_events - (Optional) Invoke the hook for push events.
-
 // push_events_branch_filter - (Optional) Invoke the hook for push events on matching branches only.
 
 // issues_events - (Optional) Invoke the hook for issues events.
 
 // confidential_issues_events - (Optional) Invoke the hook for confidential issues events.
 
-// merge_requests_events - (Optional) Invoke the hook for merge requests.
-
-// tag_push_events - (Optional) Invoke the hook for tag push events.
-
 // note_events - (Optional) Invoke the hook for notes events.
 
 // confidential_note_events - (Optional) Invoke the hook for confidential notes events.
 
 // job_events - (Optional) Invoke the hook for job events.
-
-// pipeline_events - (Optional) Invoke the hook for pipeline events.
 
 // wiki_page_events - (Optional) Invoke the hook for wiki page events.
 
--- webhooks.tf
+++ webhooks.tf
@@ -1,9 +1,13 @@
 module "webhooks" {
   source          = "./modules/webhooks"
   for_each        = var.webhooks
-  project       = var.project
+  project         = var.project
   url             = each.key
   pipeline_events = each.value.pipeline_events
+  merge_requests_events = each.value.merge_requests_events
+  push_events = each.value.push_events
+  enable_ssl_verification = each.value.enable_ssl_verification
+  tag_push_events = each.value.tag_push_events
 }
 
 variable "webhooks" {
